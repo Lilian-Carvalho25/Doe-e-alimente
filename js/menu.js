fb4983a9098a650b8ab45d1b80adfdadb5f16126
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-04-27 13:47:05 -0300

    Ajustes menu responsivo

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,11 @@ const icone = document.querySelector(".icone");
 botao.addEventListener("click", function(event){
     event.preventDefault();
     linksMenu.classList.toggle("aberto");
+
+    if(linksMenu.classList.contains("aberto")){
+        icone.innerHTML = "Fechar &times;";
+    } else {
+        icone.innerHTML = "Menu &equiv;";
+    }
 });
+
