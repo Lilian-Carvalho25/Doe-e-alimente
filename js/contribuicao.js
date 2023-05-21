ca042cce95371e32334f6d27cfd4701762f960c8
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-05-21 18:25:16 -0300

    Arrumando a responsividade
    Ajustei os dados da página home, e consertei os formulários

--- js/contribuicao.js
+++ js/contribuicao.js
@@ -1,10 +1,10 @@
 const caixaDoacao = document.querySelector("#container-selecao form");
 const campoValor = caixaDoacao.querySelector("#valor-inserido");
-const formulario = document.querySelector("#container-formulario form");
-const campoNome = formulario.querySelector("#nome");
-const campoEmail = formulario.querySelector("#email");
-const campoTelefone = formulario.querySelector("#telefone");
-const campoCelular = formulario.querySelector("#celular");
+const formulario = document.querySelector(".container-formularios form");
+const campoNome = formulario.querySelector(".nomes");
+const campoEmail = formulario.querySelector(".emails");
+const campoTelefone = formulario.querySelector(".telefones");
+const campoCelular = formulario.querySelector(".celulares");
 
 $(campoTelefone).mask("(00) 0000-0000");
 $(campoCelular).mask("(00) 0000-00000");
